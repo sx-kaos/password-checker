eadd21e6fd712490c1973695145d8b9caf828933
kaos <79645506+sx-kaos@users.noreply.github.com>
2021-10-30 19:00:08 +0100

    updated password.py
    just made the entire thing into a loop so you dont have to run it over and over
    also added a ValueError exception so if you dont input a integer or if you dont input anything it does close the program

--- password.py
+++ password.py
@@ -1,19 +1,26 @@
+import os 
+from os import system
 import datetime
 from datetime import datetime
 
+def main():
+    os.system('cls')
+    try:
+        password = int(input('Enter length of password: '))
+        now = datetime.now()
+        length = 92 ** int(password)
+        x = 1
+        print(f'{length} possible passwords of that length')
+        while True:
+            x += 1
+            if x == length:
+                end = datetime.now()
+                print(f'if the hackers started at {now} they would finish at {end}')
+                input()
+                main()
+            elif x != length:
+                pass
+    except ValueError:
+        main()
 
-password = input('Enter length of password: ')
-now = datetime.now()
-length = 92 ** int(password)
-x = 1
-print(f'{length} possible passwords of that length')
-print("this make take some time depending on the length of your password")
-while True:
-    x += 1
-    if x == length:
-        end = datetime.now()
-        print(f'if the hackers started at {now} they would finish at {end}')
-        input()
-        break
-    elif x != length:
-        pass
+main()
